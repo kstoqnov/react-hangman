e5c7f0a5b276250e2aefabd0eb68d07b3a03b442
kstoqnov <kirilstoqnov@gmail.com>
2021-08-22 00:05:35 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Hangman Games 
+Create React Hangman Games App.
+
---
+++ src/App.js
@@ -0,0 +1,116 @@
+import React, { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Figure from "./components/Figure";
+import WrongLetters from "./components/WrongLetters";
+import Word from "./components/Word";
+import Popup from "./components/Popup";
+import Notification from "./components/Notification";
+import { showNotification as show } from "./helpers/Helpers";
+import "./styles/App.css";
+
+const words = [
+  "abjad",
+  "akhirat",
+  "aksesori",
+  "aktif",
+  "akuarium",
+  "aluminium",
+  "ambulans",
+  "analisis",
+  "antena",
+  "antre",
+  "anugerah",
+  "azan",
+  "afdal",
+  "agamais",
+  "ajek",
+  "adjektif",
+  "aktual",
+  "balsam",
+  "batalion",
+  "baterai",
+  "barzakh",
+  "bazar",
+  "besok",
+  "bosan",
+  "bus",
+  "cabai",
+  "capai",
+  "cedera",
+  "cendekiawan",
+  "cengkih",
+  "cinderamata",
+  "cokelat",
+  "daftar",
+  "derajat",
+  "desain",
+  "detail",
+  "detergen",
+  "diagnosis",
+  "efektif",
+  "efektivitas",
+  "ekosistem",
+  "ekspor",
+  "ekstra",
+  "ekstrem",
+  "elite",
+];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [showNotification, setShowNotification] = useState(false);
+
+  useEffect(() => {
+    const handleKeydown = (event) => {
+      const { key, keyCode } = event;
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase();
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters((currentLetters) => [...currentLetters, letter]);
+          } else {
+            show(setShowNotification);
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters((currentLetters) => [...currentLetters, letter]);
+          } else {
+            show(setShowNotification);
+          }
+        }
+      }
+    };
+    window.addEventListener("keydown", handleKeydown);
+
+    return () => window.removeEventListener("keydown", handleKeydown);
+  }, [correctLetters, wrongLetters, playable]);
+
+  function playAgain() {
+    setPlayable(true);
+
+    // Empty Arrays
+    setCorrectLetters([]);
+    setWrongLetters([]);
+
+    const random = Math.floor(Math.random() * words.length);
+    selectedWord = words[random];
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="game-container">
+        <Figure wrongLetters={wrongLetters} />
+        <WrongLetters wrongLetters={wrongLetters} />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+      </div>
+      <Popup correctLetters={correctLetters} wrongLetters={wrongLetters} selectedWord={selectedWord} setPlayable={setPlayable} playAgain={playAgain} />
+      <Notification showNotification={showNotification} />
+    </>
+  );
+}
+
+export default App;
